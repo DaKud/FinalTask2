4644022916a660579bb469d5307061a8065f8b92
Da <106489968+DaKud@users.noreply.github.com>
2024-02-09 17:31:40 +0300

    Update MathService.java

--- Task2/src/main/java/MathService.java
+++ Task2/src/main/java/MathService.java
@@ -1,23 +1,20 @@
 public class MathService {
 
-    public Pair getAnswer(int a, int b, int c) throws NotFoundAnswerException {
-        int d = getD(a,b,c);
-        if (d < 0) {
-            throw new NotFoundAnswerException("Корни не могут быть найдены");
-        }
-        if (d == 0) {
-            Double x = (double) ((-b) / (2 * a));
-            return new Pair(x,x);
-        }
-        if (d > 0) {
-            Double x1 = (-b + Math.sqrt(d)) / (2 + a);
-            Double x2 = (-b - Math.sqrt(d)) / (2 + a);
-            return new Pair(x1, x2);
-        }
-        return null;
-    }
-
     public int getD(int a, int b, int c) {
         return b * b - 4 * a * c;
+    }
+    public Pair<Double, Double> getRoots(int a, int b, int c) throws NotFoundAnswerException {
+        int d = getD(a, b, c);
+
+        if (d < 0) {
+            throw new NotFoundAnswerException("The equation has no real roots.");
+        }
+        if (d == 0) {)
+            double x = (-b) / (2.0 * a);
+            return new Pair<>(x, x);
+        }
+        double x1 = (-b + Math.sqrt(d)) / (2.0 * a);
+        double x2 = (-b - Math.sqrt(d)) / (2.0 * a);
+        return new Pair<>(x1, x2);
     }
 }
